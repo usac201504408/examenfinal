3059879f8ddc57eaa2b5b3360f05cd1c477a1a7f
usac201504408 <3001650050101@ingenieria.usac.edu.gt>
2020-06-28 08:19:05 -0700

    desconectar socket de servidor

---
+++ cliente2/cliente/clienteTCP.py
@@ -0,0 +1,74 @@
+import socket
+import os
+import logging
+import time
+
+class clienteTCP():
+    def __init__(self,a,p,b,p1):
+        self.addr = a
+        self.port = p
+        self.buff = b
+        self.port1 = p1
+
+    def recibircliente(self): #creando metodo para recepcion de audio
+        #esto pasarlo a un metodo que haga el conect del puerto
+        sock = socket.socket()
+        sock.connect((self.addr, self.port1))
+        # sock.settimeout(30)
+
+        try:
+            buff= self.buff
+            archivo = open('../cliente/tempFiles/recibido.wav', 'wb') #Aca se guarda el archivo entrante
+            while buff:
+                buff = sock.recv(self.buff) 
+                archivo.write(buff)
+
+            archivo.close() #Se cierra el archivo
+
+            print("recepcion  de archivo finalizado")
+
+        finally:
+            print('Conexion al servidor finalizada')
+            # sock.shutdown(socket.SHUT_WR)
+            
+            sock.close() #Se cierra el socket
+
+    def enviarcliente(self, duracion): #creando metodo para envio de audio
+
+        sock = socket.socket()
+        sock.connect((self.addr, self.port1))
+        # sock.settimeout(30)
+        # segundos = input("Cuantos segundos deseas grabar? ")
+        # logging.basicConfig(
+        #     level = logging.DEBUG, 
+        #     format = '%(message)s'
+        #     )
+
+
+        # logging.info('Comenzando grabacion')
+        # os.system('arecord -d '+duracion+' -f U8 -r 8000 ../cliente/tempFiles/enviar.wav')
+        #('arecord -d '+duracion+' -f U8 -r 8000 ../cliente/tempFiles/enviar.wav')
+
+        try:
+            buff= self.buff
+            with open('../cliente/tempFiles/enviar.wav', 'rb') as archivo: #Aca se guarda el archivo entrante
+                sock.sendfile(archivo,0)
+
+            archivo.close() #Se cierra el archivo
+
+            print("envio de archivo finalizado")
+        except Exception as ex:
+            print("excepcion: " + str(ex))
+        finally:
+            print('Conexion al servidor finalizada')
+            # sock.shutdown(socket.SHUT_WR)
+            
+            sock.close() #Se cierra el socket
+
+    
+
+
+# Datos= clienteTCP('localhost' , 9800, 65495,9801)
+# Datos.recibircliente()
+# Datos.enviarcliente("5")
+#tengo 2 puertos porque tcp no me deja cerrar la conexion y reabrirla antes de 1 min
---
+++ cliente2/cliente/comandosCliente.py
@@ -0,0 +1,71 @@
+#JPGM clase para comandos de cliente
+from globalconst import *
+
+class comandosCliente(object):
+
+    def __init__(self):
+        pass
+
+    def getTrama(self, comando, variable1, variable2 = "", separador = "$"):
+       
+        
+        
+        if(comando != ""):
+
+            trama = bytes
+            #MVFC se codifica la variable para poderla sumar
+            variable1 = variable1.encode()
+            separador = separador.encode()
+            #puede venir 1, 2 o mas, yo voy a empezar a partir para armar la trama
+            if(comando == COMMAND_FTR): #transferencia archivos, usa dos variables y una constante
+                variable2 = variable2.encode()
+                trama = comando + bytes(separador) + bytes(variable1) + bytes(separador) + bytes(variable2)
+            elif(comando == COMMAND_ALIVE): #alive usa 1 variable y una constante
+                trama = comando + bytes(separador) + bytes(variable1)
+            elif(comando == COMMAND_CHAT): #comando para chat
+                trama = comando + bytes(separador) + bytes(variable1)
+            elif(comando == COMMAND_ACK): #comando para acknowledge
+                trama = comando + bytes(separador) + bytes(variable1)
+            elif(comando == COMMAND_FRR): #comando para FRR
+            
+                variable2 = variable2.encode()
+        
+                trama = comando + bytes(separador) + bytes(variable1) + bytes(separador) + bytes(variable2)
+            return trama
+        else: #es archivo de audio
+            return variable1
+            
+            
+        
+
+    def splitTramaCliente(self, trama, separador = "$"):
+        #MVFC verifico si le muestro o no al usuario este mensaje
+        #las tramas ALIVE emitidas por el cliente no se las muestro a el
+        trama = bytes(trama)
+        trama = trama.decode()
+        arregloTrama = trama.split(separador)
+        
+        return arregloTrama
+        
+            
+#codigo de test clase
+#objetoComandos = comandosCliente()
+# # # # # trama_recibida = objetoComandos.getTrama(binascii.unhexlify("05"), "201504408")
+# # # trama_chat = objetoComandos.getTrama(b'\x80', str("hola"))
+# # # print("trama chat: " + str(trama_chat))
+# tramaCLiente = objetoComandos.splitTramaCliente(b'\x08$hola', "$")
+# # print(tramaCLiente)
+# print(type(tramaCLiente[0].encode()))
+# print(type(binascii.unhexlify("08")))
+# print(type(binascii.unhexlify("04")))
+
+# if(tramaCLiente[0].encode() != binascii.unhexlify("04")):
+#     print("mensaje de texto")
+#     print(tramaCLiente[0].encode())
+# else:
+#     print(tramaCLiente[0].encode())
+# filesize = 64 * 1024
+# tramaCLiente = objetoComandos.getTrama(binascii.unhexlify("03"), str("201504408"), str(filesize))
+# print(tramaCLiente)
+# tramasplit = objetoComandos.splitTramaCliente(tramaCLiente, "$")
+# print(tramasplit)
